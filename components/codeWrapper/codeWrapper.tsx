434bf23e6aff6eb013d500b2af5b60225527eb9e
VIPIN BOHRA <vipinbohra321@gmail.com>
2023-11-17 16:55:53 +0530

    feat: added assets

---
+++ components/codeWrapper/codeWrapper.tsx
@@ -0,0 +1,113 @@
+export default function CodeWrapper() {
+  return (
+    <pre className="text-white w-[650px] border-2 border-[#333] pt-2 text-[16px] font-thin rounded-xl">
+      <nav className="block">
+        <div className="flex mb-4">
+          <p className="w-[14px] h-[14px] rounded-full bg-[#FF605C] ml-2 mr-1"></p>
+          <p className="w-[14px] h-[14px] rounded-full bg-[#FFBD44] mr-1"></p>
+          <p className="w-[14px] h-[14px] rounded-full bg-[#00CA4E]"></p>
+        </div>
+        <div className="">
+          <span className="bg-[#222] p-2 rounded-lg ml-8">my_profile.js </span>
+        </div>
+      </nav>
+      <div className="bg-[#222] mt-0">
+        <span className="block">
+          <span className="inline-block mr-8 ml-2">1</span>
+          <span className="text-[#c792ea]">const</span>{" "}
+          <span className="text-[#F4C430]">name</span>{" "}
+          <span className="text-[#89ddff]">=</span>{" "}
+          <span className="text-[#a5e844]">'Vipin Bohra'</span>
+        </span>
+        <span className="block">
+          <span className="inline-block mr-8 ml-2">2</span>
+          <span className="text-[#c792ea]">const</span>{" "}
+          <span className="text-[#F4C430]">skills</span> <span>=</span>{" "}
+          <span className="text-[#89ddff]">[</span>
+        </span>
+        <span className="block border-l-4 border-l-[#89ddff] -ml-1 bg-[#eee2]">
+          <span className="block">
+            <span className="inline-block mr-12 ml-2">3</span>
+            <span className="text-[#a5e844]">
+              'javaScript'<span className="text-[#89ddff]">,</span>
+            </span>
+          </span>
+
+          <span className="block">
+            <span className="inline-block mr-12 ml-2">4</span>
+            <span className="text-[#a5e844]">
+              'Nextjs'<span className="text-[#89ddff]">,</span>
+            </span>
+          </span>
+
+          <span className="block">
+            <span className="inline-block mr-12 ml-2">5</span>
+            <span className="text-[#a5e844]">
+              'Nodejs'<span className="text-[#89ddff]">,</span>
+            </span>
+          </span>
+
+          <span className="block">
+            <span className="inline-block mr-12 ml-2">6</span>
+            <span className="text-[#a5e844]">
+              'MongoDB'<span className="text-[#89ddff]">,</span>
+            </span>
+          </span>
+          <span className="block">
+            <span className="inline-block mr-12 ml-2">7</span>
+            <span className="text-[#a5e844]">'MySql'</span>
+          </span>
+        </span>
+        <span className="block">
+          <span className="inline-block mr-8 ml-2">8</span>
+
+          <span className=" text-[#89ddff]">]</span>
+        </span>
+        <span className="block">
+          <span className="inline-block mr-8 ml-2">9</span>
+          <span className="text-[#c792ea]">const</span>{" "}
+          <span className="text-[#F4C430]">want_to_hire</span> <span>=</span>{" "}
+          <span>true</span>
+        </span>
+        <span className="block">
+          <span className="inline-block mr-6 ml-2">10</span>
+          <span className="text-[#c792ea]">const</span>{" "}
+          <span className="text-[#F4C430]">prefect_match</span> <span>=</span>{" "}
+          <span>
+            skills.<span className="text-[#89ddff]">includes</span>
+            <span>(</span>
+            <span className="text-[#a5e844]">'React'</span>
+            <span>)</span>
+          </span>
+        </span>
+        <span className="block">
+          <span className="inline-block mr-6 ml-2">11</span>
+          <span className="text-[#c792ea]">if</span> <span>(</span>
+          <span>want_to_hire</span> <span className="text-[#ff0000]">&&</span>{" "}
+          <span>prefect_match</span>
+          <span>)</span>
+        </span>
+        <span className="block">
+          <span className="inline-block mr-16 ml-2">12</span>
+          <span className="text-[#F4C430]">console.</span>
+          <span className="text-[#89ddff]">log</span>
+          <span>(</span>
+          <span className="text-[#a5e844]">'Vola! found perfect match!!'</span>
+          <span>)</span>
+        </span>
+        <span className="block">
+          <span className="inline-block ml-2">13</span>
+        </span>
+        <span className="block">
+          <span className="inline-block ml-2">14</span>
+        </span>
+        <span className="block">
+          <span className="inline-block ml-2">15</span>
+        </span>
+        <span className="block">
+          <span className="inline-block ml-2">16</span>
+        </span>
+      </div>
+    </pre>
+  );
+}
---
+++ components/experience/experience.tsx
@@ -0,0 +1,205 @@
+import CodeWrapper from "../codeWrapper/codeWrapper";
+
+export default function Experience() {
+  return (
+    <div className="text-white min-h-screen pl-40 flex">
+      <div className=" flex items-center max-h-screen sticky top-0">
+        <CodeWrapper />
+      </div>
+      <div className="grow pl-8">
+        <h3 className="text-4xl text-center pt-32 pb-8" id="skills">
+          Skills
+        </h3>
+        <div className="text-center pb-8">
+          <span className="bg-[#FF605C] px-4 py-2 rounded-lg">Frontend</span>
+        </div>
+        <div className="w-full flex flex-wrap items-start justify-start">
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(237,199,0,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/javascript.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>javaScript</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="bg-[#aaa] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/nextjs.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>NextJs</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(128,222,234,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/react-native.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>React</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(126,87,194,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/redux.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>Redux</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(105,149,218,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/typescript.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>Typescript</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(0,172,193,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/tailwindcss.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>Tailwind</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(25,155,224,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/material-ui.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>Material Ui</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(238,194,128,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/html-5--v2.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>HTML</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(111,161,211,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img alt="html" src="./assets/css3.webp" width={50} height={50} />
+            </div>
+            <span>CSS</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(157,139,204,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/bootstrap.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>Bootstrap</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(240,98,146,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img alt="html" src="./assets/sass.webp" width={50} height={50} />
+            </div>
+            <span>SASS</span>
+          </div>
+        </div>
+        <div className="text-center pt-16 pb-8">
+          <span className="bg-[#FFBD44] px-4 py-2 rounded-lg">Backend</span>
+        </div>
+        <div className="w-full flex flex-wrap items-start justify-start">
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(59,107,77,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/nodejs.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>nodeJS</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="bg-[#aaa] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/express.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>ExpressJS</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(92,132,76,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/mongodb.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>MongoDB</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(255,179,0,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/firebase.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>Firebase</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="bg-[rgba(105,149,218,0.1)] rounded-full flex items-center justify-center w-[90px] h-[90px] m-4">
+              <img
+                alt="html"
+                src="./assets/mysql.webp"
+                width={50}
+                height={50}
+              />
+            </div>
+            <span>MySQL</span>
+          </div>
+        </div>
+
+        <div className="text-center pt-16 pb-8">
+          {/* <span className="bg-[#00CA4E] px-4 py-2 rounded-lg">Tools</span> */}
+        </div>
+      </div>
+    </div>
+  );
+}
